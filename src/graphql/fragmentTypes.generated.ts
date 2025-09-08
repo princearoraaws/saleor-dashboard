15e8d8a2511026b5ab9c7a1ddfc47f6e854d899a
princearoraaws <59504073+princearoraaws@users.noreply.github.com>
2025-09-08 07:11:35 +0000

    chore: update GraphQL schema and generated types

--- src/graphql/fragmentTypes.generated.ts
+++ src/graphql/fragmentTypes.generated.ts
@@ -11,6 +11,28 @@
       "AppExtensionOptionsNewTab",
       "AppExtensionOptionsWidget"
     ],
+    "AssignedAttribute": [
+      "AssignedBooleanAttribute",
+      "AssignedDateAttribute",
+      "AssignedDateTimeAttribute",
+      "AssignedFileAttribute",
+      "AssignedMultiCategoryReferenceAttribute",
+      "AssignedMultiChoiceAttribute",
+      "AssignedMultiCollectionReferenceAttribute",
+      "AssignedMultiPageReferenceAttribute",
+      "AssignedMultiProductReferenceAttribute",
+      "AssignedMultiProductVariantReferenceAttribute",
+      "AssignedNumericAttribute",
+      "AssignedPlainTextAttribute",
+      "AssignedSingleCategoryReferenceAttribute",
+      "AssignedSingleChoiceAttribute",
+      "AssignedSingleCollectionReferenceAttribute",
+      "AssignedSinglePageReferenceAttribute",
+      "AssignedSingleProductReferenceAttribute",
+      "AssignedSingleProductVariantReferenceAttribute",
+      "AssignedSwatchAttribute",
+      "AssignedTextAttribute"
+    ],
     "CheckoutLineProblem": [
       "CheckoutLineProblemInsufficientStock",
       "CheckoutLineProblemVariantNotAvailable"
@@ -334,6 +356,10 @@
     "OrderOrCheckout": [
       "Checkout",
       "Order"
+    ],
+    "PaymentMethodDetails": [
+      "CardPaymentMethodDetails",
+      "OtherPaymentMethodDetails"
     ],
     "PromotionEvent": [
       "PromotionCreatedEvent",
